f259b80e000cc14bfd6d8e9904d0388a99e44b5d
CidaliaA <cidalia.andre01gmail.com>
2021-10-08 15:29:18 +0200

    feat:js:elementsDOM,modelescoeurs
    git commit -m feat:js:elementsDOM,modelescoeurs

---
+++ app.js
@@ -0,0 +1,23 @@
+// elements du DOM(query1 element class;byid=id;tag=tout element selon class tablau puis element body rang zero)
+const divVies = document.querySelector('.vies');
+console.log=('.vies');
+const message = document.getElementById('message');
+console.log=('message');
+const formulaire = document.getElementById('inputBox');
+console.log=('inputBox');
+const input = document.getElementById('number');
+console.log=('number');
+const essayerBtn =document.getElementById('essayerBtn');
+console.log=('essayerBtn');
+const rejouerBtn= document.getElementById('rejouer');
+console.log=('rejouer');
+const body= document.getElementsByTagName('body')[0];
+console.log=('body');
+
+
+// modeles de coeurs
+const coeurVides = '<ion-icon name="heart-outline"></ion-icon>';
+console.log=("heart-ouline");
+const coeurPlein = '<ion-icon name="heart"></ion-icon>';
+console.log=("heart");
+
